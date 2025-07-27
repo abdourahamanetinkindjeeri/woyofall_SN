de5dc0e871293c158fbe71619682284f189a20cf
abdourahamanetinkindjeeri99 <abdourahamanetinkindjeeri99@gmail.com>
2025-07-27 23:31:45 +0000

    Correction des erreurs pour le deploiement .env

--- config/environment.php
+++ config/environment.php
@@ -3,11 +3,18 @@
 use Dotenv\Dotenv;
 
 // Charger les variables d'environnement depuis le fichier .env
-if (file_exists(__DIR__ . '/../.env')) {
-  $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$envPath = '/var/www/.env';
+if (file_exists($envPath)) {
+  $dotenv = Dotenv::createImmutable('/var/www');
   $dotenv->load();
 } else {
-  throw new \Exception('Le fichier .env est requis. Copiez env.example vers .env et configurez vos variables.');
+  // Essayer le répertoire courant si le répertoire parent n'a pas le fichier
+  if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+  } else {
+    throw new \Exception('Le fichier .env est requis. Copiez env.example vers .env et configurez vos variables.');
+  }
 }
 
 return [
